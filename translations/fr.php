5be3eb1e2f8fd46165acd06d8a6d0acaf5e8cb17
Fabien Serny <fabien@23prod.fr>
2013-10-08 16:34:48 +0200

    Adding french translations

---
+++ translations/fr.php
@@ -0,0 +1,17 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{sellermania}prestashop>sellermania_92e316a9d4cbc0574518eb35db24cb27'] = 'SellerMania';
+$_MODULE['<{sellermania}prestashop>sellermania_9094fa3db5efec617e2dba1de514274a'] = 'Connectez votre PrestaShop avec les webservices SellerMania';
+$_MODULE['<{sellermania}prestashop>displaygetcontent_92e316a9d4cbc0574518eb35db24cb27'] = 'SellerMania';
+$_MODULE['<{sellermania}prestashop>displaygetcontent_771847b3a8dd3d37f474493428ecca70'] = 'Configuration du module SellerMania';
+$_MODULE['<{sellermania}prestashop>displaygetcontent_b6fbf11efeb3341088e2df23d06028c0'] = 'Vous avez deux solutions pour envoyer votre catalogue à SellerMania';
+$_MODULE['<{sellermania}prestashop>displaygetcontent_b984240384dfae4038b9f5d4a8e46238'] = 'Mettre en place une tâche automatisée (tâche cron)';
+$_MODULE['<{sellermania}prestashop>displaygetcontent_aabfdaeee727f05e225952de0bdf4d93'] = 'Chemin du script :';
+$_MODULE['<{sellermania}prestashop>displaygetcontent_9f1aa175c291e3ad33eb64e70e184b7c'] = 'Les fichiers générés seront accessibles à ces urls :';
+$_MODULE['<{sellermania}prestashop>displaygetcontent_cef6871cec9155fbf00650a5ec69c321'] = 'Généré le';
+$_MODULE['<{sellermania}prestashop>displaygetcontent_750d5520b95ffb9c56927a66d258b8aa'] = 'Fichier non généré pour le moment';
+$_MODULE['<{sellermania}prestashop>displaygetcontent_7c231695492ff85c2b0df3fc9148f253'] = 'Attention, le répertoire suivant doit être posséder les droits en écriture';
+$_MODULE['<{sellermania}prestashop>displaygetcontent_1d00e7dce692e8dc3f6877f035e3a616'] = 'OU';
+$_MODULE['<{sellermania}prestashop>displaygetcontent_718269b9af911f6f5c0973cba5ae4e36'] = 'Envoyer ces liens à SellerMania';
