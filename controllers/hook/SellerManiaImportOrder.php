9bb7b42e27c833cbc093d8d3a97c16c17bbfe4f2
Fabien Serny <fabien@23prod.fr>
2013-12-19 12:00:30 +0100

    Create customer, address and cart

--- controllers/hook/SellerManiaImportOrder.php
+++ controllers/hook/SellerManiaImportOrder.php
@@ -47,7 +47,65 @@ class SellerManiaImportOrderController
 	 */
 	public function run($order)
 	{
+		// Set firstname and lastname
+		$names = explode(' ', $order['User'][0]['Name']);
+		$firstname = $names[0];
+		if (isset($names[1]) && !empty($names[1]) && count($names) == 2)
+			$lastname = $names[1];
+		else
+		{
+			$lastname = $order['User'][0]['Name'];
+			$lastname = str_replace($firstname.' ', '', $lastname);
+		}
 
+		// Set shipping phone
+		$shipping_phone = '0100000000';
+		if (isset($order['User'][0]['Address']['ShippingPhone']) && !empty($order['User'][0]['Address']['ShippingPhone']))
+			$shipping_phone = $order['User'][0]['Address']['ShippingPhone'];
+
+		// Set currency
+		$currency_iso_code = 'EUR';
+		if (isset($order['OrderInfo']['Amount']['Currency']))
+			$currency_iso_code = $order['OrderInfo']['Amount']['Currency'];
+
+
+		// Create customer as guest
+		$customer = new Customer();
+		$customer->id_gender = 9;
+		$customer->firstname = $firstname;
+		$customer->lastname = $lastname;
+		$customer->email = Configuration::get('PS_SHOP_EMAIL');
+		$customer->passwd = md5(pSQL(_COOKIE_KEY_.rand()));
+		$customer->is_guest = 1;
+		$customer->active = 1;
+		$customer->add();
+
+		// Create address
+		$address = new Address();
+		$address->alias = 'Sellermania';
+		$address->firstname = $firstname;
+		$address->lastname = $lastname;
+		$address->address1 = $order['User'][0]['Address']['Street1'];
+		$address->address2 = $order['User'][0]['Address']['Street2'];
+		$address->postcode = $order['User'][0]['Address']['ZipCode'];
+		$address->city = $order['User'][0]['Address']['City'];
+		$address->id_country = Country::getByIso($order['User'][0]['Address']['Country']);
+		$address->phone = $shipping_phone;
+		$address->id_customer = $customer->id;
+		$address->active = 1;
+		$address->add();
+
+		// Create Cart
+		$customer_cart = new Cart();
+		$customer_cart->id_customer = $customer->id;
+		$customer_cart->id_address_invoice = $address->id;
+		$customer_cart->id_address_delivery = $address->id;
+		$customer_cart->id_carrier = 0;
+		$customer_cart->id_lang = $customer->id_lang;
+		$customer_cart->id_currency = Currency::getIdByIsoCode($currency_iso_code);
+		$customer_cart->recyclable = 0;
+		$customer_cart->gift = 0;
+		$customer_cart->add();
 	}
 }
 
