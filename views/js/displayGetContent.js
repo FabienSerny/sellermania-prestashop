c8d55024519477dbf2bd53af6725a54161bad2af
Fabien Serny <fabien@23prod.fr>
2013-12-29 00:02:24 +0100

    Fix JS compatibility for PS 1.4

--- views/js/displayGetContent.js
+++ views/js/displayGetContent.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     function displaySellermaniaCredentials()
     {
-        if ($('#sm_import_orders_yes').attr('checked') == 'checked')
+        if ($('#sm_import_orders_yes').attr('checked') == 'checked' || $('#sm_import_orders_yes').attr('checked') == true)
             $('#sm_import_orders_credentials').fadeIn();
         else
             $('#sm_import_orders_credentials').fadeOut();
