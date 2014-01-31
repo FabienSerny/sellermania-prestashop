5b863b22d1e9998d50e15e7fa0295fa3662f6126
Fabien Serny <fabien@23prod.fr>
2014-01-31 03:14:25 +0100

    Display order item status field and registration

--- views/js/displayAdminOrder.js
+++ views/js/displayAdminOrder.js
@@ -53,11 +53,80 @@ $(document).ready(function() {
                     html_order_line += '<u>Ean:</u> <b>' + sellermania_products[sku].ean + '</b><br>';
                     html_order_line += '<u>Condition:</u> <b>' + sellermania_products[sku].item_condition + '</b><br>';
                     html_order_line += '<u>Status:</u> <b>' + sellermania_products[sku].status + '</b><br>';
+                    if (sellermania_products[sku].status_id == 6)
+                    {
+                        html_order_line += '<input type="radio" id="status_confirm_' + sellermania_order_line + '" name="status_' + sellermania_order_line + '" value="1" class="status_order_line" data-toggle="' + sellermania_products[sku].order_item_id + '" /> Confirm ';
+                        html_order_line += '<input type="radio" id="status_cancel_' + sellermania_order_line + '" name="status_' + sellermania_order_line + '" value="-1" class="status_order_line" data-toggle="' + sellermania_products[sku].order_item_id + '" /> Cancel ';
+                    }
                     $(this).html(html_order_line);
                 }
                 sellermania_order_row++;
             });
         }
         sellermania_order_line++;
+    });
+
+    // Check status
+    $('.status_order_line').click(function() {
+
+        // Fill product details
+        var sellermania_status_not_defined = 0;
+        var sellermania_order_line = 0;
+
+        $('#orderProducts tr').each(function() {
+            if (sellermania_order_line > 0)
+            {
+                var sellermania_order_row = 0;
+                $(this).find('td').each(function() {
+                    if (sellermania_order_row == 1 && $(this).is(':visible'))
+                    {
+                        // Retrieve sku
+                        var sku = $(this).html().split('<br>');
+                        sku = sku[1].replace('Ref: ', '').trim();
+
+                        // Status
+                        var order_line_status = 'Not defined';
+                        if ($('#status_confirm_' + sellermania_order_line).prop('checked'))
+                            order_line_status = 'Confirmed';
+                        if ($('#status_cancel_' + sellermania_order_line).prop('checked'))
+                            order_line_status = 'Cancelled';
+
+                        // Count not defined Status
+                        if (order_line_status == 'Not defined')
+                            sellermania_status_not_defined++;
+                    }
+                    sellermania_order_row++;
+                });
+            }
+            sellermania_order_line++;
+        });
+
+        // Check how many not defined status there is
+        if (sellermania_status_not_defined == 0)
+        {
+            // Display submit
+            $('.add_product').parent().next().next().next().next().next().html('<input type="button" id="sellermania_register_status" value="Register status" class="button" />');
+
+            // Generate form and submit it
+            $('#sellermania_register_status').click(function() {
+
+                // Generate form
+                var html_form = '<form id="sellermania_status_form" action="" method="POST">';
+                $('.status_order_line').each(function() {
+                    if ($(this).prop('checked'))
+                    {
+                        html_form += '<input type="hidden" name="' + $(this).attr('name') + '" value="' + $(this).attr('value') + '" />';
+                        html_form += '<input type="hidden" name="order_item_id_' + $(this).attr('name') + '" value="' + $(this).attr('data-toggle') + '" />';
+                    }
+                });
+                html_form += '</form>';
+
+                // Display form and submit
+                $('.add_product').parent().next().next().next().next().next().html(html_form);
+                $('#sellermania_status_form').submit();
+
+                return false;
+            });
+        }
     });
 });
