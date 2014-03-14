40d68da6826b7ab4fb725bce10133694f9b8f1c4
Fabien Serny <fabien@23prod.fr>
2014-03-14 17:10:21 +0100

    Add code comment

--- views/js/displayBackOfficeHeader.js
+++ views/js/displayBackOfficeHeader.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 
+    // Hide PDF invoice link on orders list when the order status contains "Sellermania"
     var row = 0;
     $('.order tr').each(function() {
 
         var column = 0;
         var flag_sellermania = 0;
+
         $(this).find('td').each(function() {
             if (column == 7 && $(this).text().trim().toLowerCase().indexOf("sellermania") >= 0)
                 flag_sellermania = 1;
