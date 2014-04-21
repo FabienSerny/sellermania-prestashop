5089f648292db015acd4e6c5346effb7d7e23c7a
Fabien Serny <fabien@23prod.fr>
2014-04-21 15:26:19 -0500

    Code header updated (year + copyright)

--- classes/SellermaniaOrder.php
+++ classes/SellermaniaOrder.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* 2010 - 2013 Sellermania / 23Prod SARL
+* 2010 - 2014 Sellermania / Froggy Commerce / 23Prod SARL
 *
 * NOTICE OF LICENSE
 *
@@ -10,18 +10,22 @@
 * http://opensource.org/licenses/afl-3.0.php
 * If you did not receive a copy of the license and are unable to
 * obtain it through the world-wide-web, please send an email
-* to fabien@23prod.com so we can send you a copy immediately.
+* to team@froggy-commerce.com so we can send you a copy immediately.
 *
 * DISCLAIMER
 *
 * Do not edit or add to this file if you wish to upgrade your module to newer
 * versions in the future.
 *
-*  @author Fabien Serny - 23Prod <fabien@23prod.com>
-*  @copyright	2010-2013 23Prod SARL
+*  @author Fabien Serny - Froggy Commerce <team@froggy-commerce.com>
+*  @copyright	2010-2014 Sellermania / Froggy Commerce / 23Prod SARL
 *  @version		1.0
 *  @license		http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 */
+
+// Security
+if (!defined('_PS_VERSION_'))
+	exit;
 
 class SellermaniaOrder extends ObjectModel
 {
--- views/js/displayBackOfficeHeader-14.js
+++ views/js/displayBackOfficeHeader-14.js
@@ -1,3 +1,27 @@
+/*
+ * 2010 - 2014 Sellermania / Froggy Commerce / 23Prod SARL
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to team@froggy-commerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade your module to newer
+ * versions in the future.
+ *
+ *  @author Fabien Serny - Froggy Commerce <team@froggy-commerce.com>
+ *  @copyright	2010-2014 Sellermania / Froggy Commerce / 23Prod SARL
+ *  @version		1.0
+ *  @license		http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+
 $(document).ready(function() {
 
     // Hide PDF invoice link on orders list when the order status contains "Sellermania"
