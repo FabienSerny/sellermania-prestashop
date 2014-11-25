d5ce2533323c918d4ef3febb3fb5b656d800714e
Fabien Serny <fabien@23prod.fr>
2014-11-25 17:00:10 +0100

    Move the SyncStock option is another function

--- controllers/hook/SellerManiaActionValidateOrder.php
+++ controllers/hook/SellerManiaActionValidateOrder.php
@@ -50,11 +50,14 @@ class SellerManiaActionValidateOrderController
 	 */
 	public function run()
 	{
+		// Sync stock by reference (option set by the merchant)
+		$this->syncStockByReference();
+
 		// If we are in the import orders context, we do not update anything
 		if (defined('SELLERMANIA_IMPORT_ORDERS_CONTEXT'))
 			return false;
 
-		// Else we retrieve the SKU
+		// Else (Sellermania orders only) we retrieve the SKU
 		$skus = array();
 		$skus_quantities = array();
 		$products = $this->params['order']->getProducts();
@@ -64,13 +67,21 @@ class SellerManiaActionValidateOrderController
 			$skus_quantities[$product['product_reference']] = - ($product['product_quantity']);
 		}
 
-		// If the merchant use the synchronisation options
+		// We synchronize the stock
+		//$this->syncStock('ORDER', $this->params['order']->id, $skus, $skus_quantities);
+	}
+
+	public function syncStockByReference()
+	{
+		// If the merchant use the synchronisation option, for all orders (PrestaShop & Sellermania)
 		if (Configuration::get('SM_STOCK_SYNC_OPTION') == 'yes' && (int)Configuration::get('SM_STOCK_SYNC_NB_CHAR') > 0)
 		{
-			// We run again over the products
+			// We ran over the products
+			$skus_quantities = array();
+			$products = $this->params['order']->getProducts();
 			foreach ($products as $product)
 			{
-				// We retrieve the X firt or last characters of the reference of each product
+				// We retrieve the X first or last characters of the reference of each product
 				$refcode = substr($product['product_reference'], 0, (int)Configuration::get('SM_STOCK_SYNC_NB_CHAR'));
 				$sql_filter = "`reference` LIKE '".$refcode."%'";
 				if (Configuration::get('SM_STOCK_SYNC_POSITION') == 'last')
@@ -83,22 +94,31 @@ class SellerManiaActionValidateOrderController
 				if (!empty($refcode))
 				{
 					// We try to find a match in product and product_attribute tables
-					$matched_products1 = Db::getInstance()->executeS('SELECT `reference` FROM `'._DB_PREFIX_.'product` WHERE '.$sql_filter);
-					$matched_products2 = Db::getInstance()->executeS('SELECT `reference` FROM `'._DB_PREFIX_.'product_attribute` WHERE '.$sql_filter);
+					$matched_products1 = Db::getInstance()->executeS('SELECT `id_product`, 0 as `id_product_attribute`, `reference` FROM `'._DB_PREFIX_.'product` WHERE '.$sql_filter);
+					$matched_products2 = Db::getInstance()->executeS('SELECT `id_product`, `id_product_attribute`, `reference` FROM `'._DB_PREFIX_.'product_attribute` WHERE '.$sql_filter);
 					$matched_products = array_merge($matched_products1, $matched_products2);
 
 					// Then we continue to fill the skus arrays
 					foreach ($matched_products as $match)
-					{
-						$skus[] = $match['product_reference'];
-						$skus_quantities[$match['product_reference']] = - ($product['product_quantity']);
-					}
+						if ($match['id_product'] != $product['product_id'] && $match['id_product_attribute'] != $product['product_attribute_id'])
+						{
+							$skus[] = $match['product_reference'];
+							$skus_quantities[$match['product_reference']] = - ($product['product_quantity']);
+
+							// If it's not a product combination, we update the product table, else, we update the product_attribute table
+							if ($match['id_product_attribute'] == 0)
+								Db::getInstance()->execute('UPDATE `'._DB_PREFIX_.'product` SET `quantity` = `quantity` - '.(int)$product['product_quantity'].' WHERE `id_product` = '.(int)$match['id_product']);
+							else
+								Db::getInstance()->execute('UPDATE `'._DB_PREFIX_.'product_attribute` SET `quantity` = `quantity` - '.(int)$product['product_quantity'].' WHERE `id_product` = '.(int)$match['id_product'].' AND `id_product_attribute` = '.(int)$match['id_product_attribute']);
+
+							// If we are on not on 1.4
+							if (substr($this->module->version, 0, 3) != '1.4')
+								Db::getInstance()->execute('UPDATE `'._DB_PREFIX_.'stock_available` SET `quantity` = `quantity` - '.(int)$product['product_quantity'].' WHERE `id_product` = '.(int)$match['id_product'].' AND `id_product_attribute` = '.(int)$match['id_product_attribute']);
+						}
 				}
 			}
+			//$this->syncStock('ORDER', $this->params['order']->id, $skus, $skus_quantities);
 		}
-
-		// We synchronize the stock
-		$this->syncStock('ORDER', $this->params['order']->id, $skus, $skus_quantities);
 	}
 
 	public function syncStock($type, $id, $new_skus, $new_skus_quantities)
