dd80782ef7d04a18ee90df0f0b81254b5152ff32
Fabien Serny <fabien@23prod.fr>
2016-05-23 17:22:11 +0200

    Update norm

--- classes/FroggyHelperTreeCategories.php
+++ classes/FroggyHelperTreeCategories.php
@@ -1,29 +1,34 @@
 <?php
-/**
- * 2013-2015 Froggy Commerce
- *
- * NOTICE OF LICENSE
- *
- * You should have received a licence with this module.
- * If you didn't buy this module on Froggy-Commerce.com, ThemeForest.net
- * or Addons.PrestaShop.com, please contact us immediately : contact@froggy-commerce.com
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to benefit the updates
- * for newer PrestaShop versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
- *
- * @author    Froggy Commerce <contact@froggy-commerce.com>
- * @copyright 2013-2015 Froggy Commerce
- * @license   Unauthorized copying of this file, via any medium is strictly prohibited
- */
+/*
+* 2010-2016 Sellermania / Froggy Commerce / 23Prod SARL
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to team@froggy-commerce.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade your module to newer
+* versions in the future.
+*
+*  @author Froggy Commerce <support@froggy-commerce.com>
+*  @copyright 2010-2016 Sellermania / Froggy Commerce / 23Prod SARL
+*  @version   1.0
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*/
 
 /*
  * Security
  */
-if (!defined('_PS_VERSION_'))
-	exit;
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
 
 class FroggyHelperTreeCategories
 {
