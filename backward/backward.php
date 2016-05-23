76f986e199c047b20487af88abccd2865376e550
Fabien Serny <fabien@23prod.fr>
2016-05-23 17:31:58 +0200

    Update norm

--- backward/backward.php
+++ backward/backward.php
@@ -17,10 +17,10 @@
 * Do not edit or add to this file if you wish to upgrade your module to newer
 * versions in the future.
 *
-*  @author Fabien Serny - Froggy Commerce <team@froggy-commerce.com>
-*  @copyright	2010-2016 Sellermania / Froggy Commerce / 23Prod SARL
-*  @version		1.0
-*  @license		http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  @author         Froggy Commerce <team@froggy-commerce.com>
+*  @copyright      2010-2016 Sellermania / Froggy Commerce / 23Prod SARL
+*  @version        1.0
+*  @license        http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 */
 
 /**
@@ -30,33 +30,33 @@
 
 // Get out if the context is already defined
 if (!in_array('Context', get_declared_classes()))
-	require_once(dirname(__FILE__).'/Context.php');
+    require_once(dirname(__FILE__).'/Context.php');
 else
 {
-	if (!in_array('ShopBackwardModuleUpdated', get_declared_classes()))
-	{
-		require_once(dirname(__FILE__).'/ContextUpdated.php');
-		$context = Context::getContext();
-		$context->shop = new ShopBackwardModuleUpdated();
-	}
+    if (!in_array('ShopBackwardModuleUpdated', get_declared_classes()))
+    {
+        require_once(dirname(__FILE__).'/ContextUpdated.php');
+        $context = Context::getContext();
+        $context->shop = new ShopBackwardModuleUpdated();
+    }
 }
 
 // Get out if the Display (BWDisplay to avoid any conflict)) is already defined
 if (!in_array('BWDisplay', get_declared_classes()))
-	require_once(dirname(__FILE__).'/Display.php');
+    require_once(dirname(__FILE__).'/Display.php');
 
 // If not under an object we don't have to set the context
 if (!isset($this))
-	return;
+    return;
 else if (isset($this->context))
 {
-	// If we are under an 1.5 version and backoffice, we have to set some backward variable
-	if (_PS_VERSION_ >= '1.5' && isset($this->context->employee->id) && $this->context->employee->id)
-	{
-		global $currentIndex;
-		$currentIndex = AdminController::$currentIndex;
-	}
-	return;
+    // If we are under an 1.5 version and backoffice, we have to set some backward variable
+    if (_PS_VERSION_ >= '1.5' && isset($this->context->employee->id) && $this->context->employee->id)
+    {
+        global $currentIndex;
+        $currentIndex = AdminController::$currentIndex;
+    }
+    return;
 }
 
 $this->context = Context::getContext();
--- views/templates/hook/index.php
+++ views/templates/hook/index.php
@@ -17,18 +17,18 @@
 * Do not edit or add to this file if you wish to upgrade your module to newer
 * versions in the future.
 *
-*  @author Fabien Serny - Froggy Commerce <team@froggy-commerce.com>
-*  @copyright	2010-2016 Sellermania / Froggy Commerce / 23Prod SARL
-*  @version		1.0
-*  @license		http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  @author         Froggy Commerce <team@froggy-commerce.com>
+*  @copyright      2010-2016 Sellermania / Froggy Commerce / 23Prod SARL
+*  @version        1.0
+*  @license        http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 */
-				    	
+
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
-						
+
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-						
+
 header("Location: ../");
 exit;
