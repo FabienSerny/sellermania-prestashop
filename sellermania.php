eb2271a09738661c6f1eb0befced4950ca86a309
Fabien Serny <fabien@23prod.com>
2017-04-21 12:00:21 +0200

    Display all carriers in configuration form

--- sellermania.php
+++ sellermania.php
@@ -39,7 +39,7 @@ class Sellermania extends Module
         $this->name = 'sellermania';
         $this->tab = 'advertising_marketing';
         $this->author = 'Froggy Commerce';
-        $this->version = '2.0.18';
+        $this->version = '2.0.19';
         $this->need_instance = 0;
 
         parent::__construct();
