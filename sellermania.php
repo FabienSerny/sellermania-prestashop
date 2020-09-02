699d17529e56ba5341c0adceeaa1d14af064a728
Fabien Serny <fabien@23prod.com>
2020-09-02 11:09:54 +0200

    Fix order importation on PS 1.4

--- sellermania.php
+++ sellermania.php
@@ -84,7 +84,7 @@ class Sellermania extends Module
         $this->name = 'sellermania';
         $this->tab = 'advertising_marketing';
         $this->author = 'Froggy Commerce';
-        $this->version = '2.5.3.8';
+        $this->version = '2.5.3.9';
         $this->need_instance = 0;
 
         parent::__construct();
