777e1204b833f07a7475666078d44ae1fc74dc7b
Fabien Serny <fabien@23prod.com>
2021-05-26 03:46:16 +0200

    Remove inventory endpoint update with v3 update

--- classes/SellermaniaInstaller.php
+++ classes/SellermaniaInstaller.php
@@ -241,7 +241,6 @@ class SellermaniaInstaller
 
         if (Tools::getValue('migrate') == 'v3') {
             Configuration::updateValue('SM_ORDER_ENDPOINT', 'http://api.sellermania.com/v3/OrdersAPIS?wsdl');
-            Configuration::updateValue('SM_INVENTORY_ENDPOINT', 'http://api.sellermania.com/v3/InventoryAPIS?wsdl');
             Configuration::updateValue('SM_API_VERSION', 'v3');
             $this->migrateMarketplacesHistory();
             Configuration::updateValue('SM_VERSION', $this->module->version);
