f85a158121db3039288a9f0d0a61cf8be6b17f76
Fabien Serny <fabien@23prod.com>
2022-09-19 19:13:49 +0200

    Update version

--- sellermania.php
+++ sellermania.php
@@ -45,7 +45,7 @@ class Sellermania extends Module
         $this->name = 'sellermania';
         $this->tab = 'advertising_marketing';
         $this->author = 'Froggy Commerce';
-        $this->version = '2.9.3';
+        $this->version = '2.9.4';
         $this->need_instance = 0;
 
         parent::__construct();
